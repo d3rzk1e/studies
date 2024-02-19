9c84a207d75ba9a2273d015c74eeff31476742f6
d3rzk1e <derzkie1994@gmail.com>
2024-02-19 17:09:45 +0300

    добавил функцию

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
 import string
 
+
 def generate_password():
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(password_characters) for _ in range(16))
+    return password
