f612d7b390c17ff281106a1b806aaf1092118828
d3rzk1e <derzkie1994@gmail.com>
2024-02-19 17:19:38 +0300

    вывод функции

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ def generate_password():
     password_characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(password_characters) for _ in range(16))
     return password
+
+
+password = generate_password()
+print(password)
